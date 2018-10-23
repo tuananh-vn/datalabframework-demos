7c22ffb32f405d65b3d15bb6d68ad6d1224f7259
Tuan Anh <anh.dt@teko.vn>
2018-10-23 09:42:13 +0700

    - add oracle docker compose

--- demos/oracle-hdfs/demo/src/ingest_py.py
+++
@@ -1,5 +0,0 @@
-import datalabframework as dlf
-dlf.project.profile('ingest-prod')
-dlf.utils.pretty_print(dlf.params.metadata())
-engine = dlf.engines.get('spark')
-spark = engine.context()
---
+++ docker/compose/oracle/data/2-sakila-data.sql
@@ -0,0 +1,11 @@
+--
+-- Dumping data for table actor
+CONNECT system/oracle;
+INSERT INTO actors VALUES (1,'PENELOPE','GUINESS',systimestamp);
+INSERT INTO actors VALUES (2,'NICK','WAHLBERG',systimestamp);
+INSERT INTO actors VALUES (3,'ED','CHASE',systimestamp);
+INSERT INTO actors VALUES (4,'JENNIFER','DAVIS',systimestamp);
+INSERT INTO actors VALUES (5,'JOHNNY','LOLLOBRIGIDA',systimestamp);
+INSERT INTO actors VALUES (6,'BETTE','NICHOLSON',systimestamp);
+INSERT INTO actors VALUES (7,'GRACE','MOSTEL',systimestamp);
+INSERT INTO actors VALUES (8,'MATTHEW','JOHANSSON',systimestamp);
